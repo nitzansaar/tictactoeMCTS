f1e030e69e816e93718d4e2bf9a2716e044388df
Nitzan Saar <nitzansaar1@gmail.com>
2025-11-04 14:28:43 -0800

    Add game history files and images for enhanced tracking - Added two new image files for game representations: `1kgames_res_4by4.png` and `100kgames_res_3by3.png`. - Introduced a comprehensive game history text file `bot_vs_random_games_readable.txt` that logs detailed game states, moves, and outcomes for 1000 games played between the bot and a random player. - Updated self-play logic to ensure accurate tracking of player actions and game results.

--- src/selfplay.py
+++ src/selfplay.py
@@ -23,11 +23,12 @@ training_dataset = TrainingDataset()
 for game_number in tqdm(range(num_games),total=num_games): # play 2500 games
     node = root_node # start with an empty board
     dataset = []
+    player = 1  # initialize player (game starts with player 1)
     while game.win_or_draw(node.state) == None: # while the game is not over
         parent_state = copy(node.state)
         node = mcts.run_simulation(root_node=node, num_simulations=1600, player=player) # run mcts to find the best action
         action, node, action_probs = mcts.select_move(node=node, mode="explore", temperature=1) # select the action with the probability of the visits
-        dataset.append([parent_state, action_probs, player])
+        dataset.append([parent_state, action_probs, player]) # board state, action probabilities, player
         player = -1 * player # switch player
     winner = game.get_reward_for_next_player(node.state,player) # assign value based on the winner
     training_dataset.add_game_to_training_dataset(dataset,winner)
